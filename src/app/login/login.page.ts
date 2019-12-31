37c3fab3df1efabe01751038cb32e3ca335f2de6
Mateus Esdras <matt.esdras@gmail.com>
2019-12-31 02:36:45 -0300

    Implementação inicial do app, no padrão 'Tabs'. Implementação inicial da tela e formulário de login. Definição do tema do app.

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CredenciaisDTO } from 'src/model/credenciais.dto';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  formGroup: FormGroup;
+  credenciais: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private nav: NavController) {
+
+    this.formGroup = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      senha: ["", [Validators.required]]
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  login(){
+    this.nav.navigateForward("/tabs/tab1");
+  }
+
+}
